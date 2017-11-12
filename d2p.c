abbb5d9ad9411b31bbedfb9455ed68bd773a0c36
JulienQC <julien.cusset33@gmail.com>
2017-11-12 16:29:51 -0500

    Fusion paralelle OK. Reste a paralelliser l'execution

--- d2p.c
+++ d2p.c
@@ -1,6 +1,37 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <assert.h>
+
+int rechercheDichotomique(int e, const int * t, int p, int r);
+
+
+int testDicho(){  
+  int t1[3] = {1,1,1};
+  int t2[4] = {0,1,2,3};
+  int t3[7] = {0,1,1,1,2,2,3};
+
+  assert(rechercheDichotomique(0, t1, 0, 3) == 0);
+  assert(rechercheDichotomique(1, t1, 0, 3) == 0);
+  assert(rechercheDichotomique(2, t1, 0, 3) == 3);
+  int i;
+  for(i = 0; i < 4; i++){
+    assert(rechercheDichotomique(i, t2, 0, 4) == i);
+  }
+  assert(rechercheDichotomique(1, t3, 0, 7) == 1);
+  assert(rechercheDichotomique(2, t3, 0, 7) == 4);
+  printf("Dichotomie OK\n");
+}
+
+int isSorted(int * t, int n){
+  int i;
+  for(i = 0; i < n - 1; i++){
+    if(t[i] > t[i + 1]){
+      return 0;
+    }
+  }
+  return 1;
+}
 
 
 void printTab(const int * t, int a, int b){
@@ -17,6 +48,11 @@ int rechercheDichotomique(int e, const int * t, int p, int r){
   // sinon, retourne l'indice i du premier element tel que t[i] >= e
   // si tous les elements sont plus petits, retourner p
 
+  if(p == r){
+    //si l'intervalle est vide, retourner le premier index
+    return p;
+  }
+  
   int i = (p + r) / 2;
   while(t[i] != e){
     if(t[i] > e){ //l'element devrait etre a gauche
@@ -29,6 +65,10 @@ int rechercheDichotomique(int e, const int * t, int p, int r){
     }
     i = (p + r) / 2;
   }
+
+  //si l'element est trouve, on verifie qu'il n'y a pas d'elements de meme
+  //valeur a un indice inferieur
+  while(i > 0 && t[i - 1] == t[i]){i--;}
   return i;
 }
 
@@ -60,16 +100,6 @@ void fusionRec(const int * t, int * r, int p1, int r1, int p2, int r2, int p3){
   fusionRec(t, r, q1 + 1, r1, q2, r2, q3 + 1);  
 }
 
-int isSorted(int * t, int n){
-  int i;
-  for(i = 0; i < n - 1; i++){
-    if(t[i] > t[i + 1]){
-      return 0;
-    }
-  }
-  return 1;
-}
-
 void fusion(int * t, int n){
 
   int * r = malloc(2 * n * sizeof(int));
@@ -93,6 +123,8 @@ void fusion(int * t, int n){
 }
 
 
+
+
 int main(int argc, char* argv[]){
   
   int n;
@@ -104,10 +136,9 @@ int main(int argc, char* argv[]){
       scanf("%d", &t[i]);
   }
 
-  putchar('\n');
   fusion(t, n);
 
   free(t);
-
+ 
   return EXIT_SUCCESS;
 }
